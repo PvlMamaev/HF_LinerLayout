4932445ec5cdfe8e8f3654176b803add5ef2ed90
PvlMamaev <95972919+PvlMamaev@users.noreply.github.com>
2024-02-20 17:23:12 +0300

    Update MainActivity
    Add link on editText. Commit with help GitHub Desktop

--- app/src/main/java/com/hfad/linearlayoutexample/MainActivity.kt
+++ app/src/main/java/com/hfad/linearlayoutexample/MainActivity.kt
@@ -2,10 +2,13 @@ package com.hfad.linearlayoutexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editText = findViewById<TextView>(R.id.edit_text_1)
     }
 }
